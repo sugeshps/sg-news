1bfc6748e985a28f016643d9b4ae6f086772ddb3
Abhilash Asokan <abhilash.asokan@logicline.de>
2019-11-09 12:34:35 +0530

    changes

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../api.service';
+import { ApiService } from '../service/api.service';
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.scss']
 })
 export class NewsComponent implements OnInit {
-articles;
-  constructor(private apiService: ApiService) { }
+  articles;
+  constructor(private apiService: ApiService) {}
 
-   ngOnInit() {
-    this.apiService.getNews().subscribe((data)=>{
+  ngOnInit() {
+    this.apiService.getNews().subscribe(data => {
       console.log(data);
       this.articles = data;
     });
